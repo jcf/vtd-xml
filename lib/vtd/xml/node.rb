1945cf329a4d57f7c301fa471dd87237f9098bc7
James Conroy-Finn <james@logi.cl>
2013-02-17 00:32:28 +0000

    Optimise Node implementation
    By lazily fetching the node name and node text we can shave 5+ seconds
    off walking a 100 MB XML file.
    
    Move attribute methods in to a module. They're ugly and I should
    refactor them but for now we'll hide them.

--- lib/vtd/xml/node.rb
+++ lib/vtd/xml/node.rb
@@ -1,72 +1,30 @@
+require 'vtd/xml/node/attributes'
+
 module VTD
   module Xml
     class Node
-      AttributeMissing = Class.new(StandardError)
+      include Attributes
 
       attr_reader :name, :text
 
       def initialize(nav, auto_pilot, current)
-        @nav = nav
+        @nav        = nav
         @auto_pilot = auto_pilot
-        @current = current
-
-        @name = @nav.to_string(@current)
-        @text = nil
-        @attributes = nil
-
-        get_text
+        @current    = current
       end
 
-      def attributes
-        @attributes ||=
-          begin
-            cache = {}
-            @auto_pilot.select_attr('*')
-
-            while (i = @auto_pilot.iterate_attr) != -1
-              cache[string_from_index i] = string_from_index i + 1
-            end
-
-            cache
-          end
+      def name
+        @name ||= @nav.to_string(@current)
       end
 
-      def [](key)
-        find_attribute(key)
-      end
-
-      def slice(*keys)
-        Hash[*keys.flat_map { |key| [key, find_attribute(key)] }]
-      end
-
-      def fetch(*args)
-        if args.length == 2
-          key, default = *args
-        else
-          key = args.first
-        end
-
-        if item = find_attribute(key)
-          return item
-        end
-
-        return yield(key) if block_given?
-        return default if defined? default
-        raise AttributeMissing, 'attribute not found'
+      def text
+        @text ||= string_from_index @nav.get_text
       end
 
       private
 
       def string_from_index(index)
         @nav.to_normalized_string(index) if index != -1
-      end
-
-      def find_attribute(key)
-        string_from_index @nav.get_attr_val(key)
-      end
-
-      def get_text
-        string_from_index @nav.get_text
       end
     end
   end
---
+++ lib/vtd/xml/node/attributes.rb
@@ -0,0 +1,53 @@
+module VTD
+  module Xml
+    class Node
+      module Attributes
+        AttributeMissing = Class.new(StandardError)
+
+        def attributes
+          @attributes ||=
+            begin
+              cache = {}
+              @auto_pilot.select_attr('*')
+
+              while (i = @auto_pilot.iterate_attr) != -1
+                cache[string_from_index i] = string_from_index i + 1
+              end
+
+              cache
+            end
+        end
+
+        def [](key)
+          find_attribute(key)
+        end
+
+        def slice(*keys)
+          Hash[*keys.flat_map { |key| [key, find_attribute(key)] }]
+        end
+
+        def fetch(*args)
+          if args.length == 2
+            key, default = *args
+          else
+            key = args.first
+          end
+
+          if item = find_attribute(key)
+            return item
+          end
+
+          return yield(key) if block_given?
+          return default if defined? default
+          raise AttributeMissing, 'attribute not found'
+        end
+
+        private
+
+        def find_attribute(key)
+          string_from_index @nav.get_attr_val(key)
+        end
+      end
+    end
+  end
+end
